d0a7f15c8c99a98ab21c6ea8159353b83ebaba36
Sri Harsha Chilakapati <hello@goharsha.com>
2020-12-12 21:51:13 +0530

    Default attributes for text; add outlets and actions; implement cancel button

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setTextAttributes(for: topTextField)
+        setTextAttributes(for: bottomTextField)
+
+        setDefaultState()
     }
 
+    private func setTextAttributes(for textField: UITextField) {
+        let textAttributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.strokeWidth: -1.0
+        ]
 
+        let existingAttributes = textField.defaultTextAttributes
+        textField.defaultTextAttributes = textAttributes.merging(existingAttributes) { (current, _) in current }
+    }
+
+    private func setDefaultState() {
+        imageView.image = nil
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+    }
+
+    @IBAction func onCameraClicked() {
+        // TODO
+    }
+
+    @IBAction func onAlbumClicked() {
+        // TODO
+    }
+
+    @IBAction func onShareClicked() {
+        // TODO
+    }
+
+    @IBAction func onCancelClicked() {
+        setDefaultState()
+    }
 }
-
