5a4e96d7aa4885fcda8b88fb064fc3e21b19275d
Sri Harsha Chilakapati <hello@goharsha.com>
2020-12-13 02:21:18 +0530

    Move keyboard up for bottomTextField

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -24,6 +24,46 @@ class ViewController: UIViewController {
         setDefaultState()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeyboardNotifications()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
+    }
+
+    private func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        if !bottomTextField.isFirstResponder {
+            return
+        }
+
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+
+    private func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        if !bottomTextField.isFirstResponder {
+            return
+        }
+
+        view.frame.origin.y = 0
+    }
+
     private func setupTextField(_ textField: UITextField) {
         textField.delegate = textFieldDelegate
 
